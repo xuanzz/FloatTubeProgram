29aeffe5cd5c3a2ef6a56f276e04f3a91a584b94
Darren Chan <xuanzz@msn.com>
2024-04-18 21:34:21 +0800

    first commit

---
+++ FloatTubeProgram.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include "MS5837.h"
+
+MS5837 sensor;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  // Initialize pressure sensor
+  // Returns true if initialization was successful
+  // We can't continue with the rest of the program unless we can initialize the sensor
+  while (!sensor.init()) {
+    Serial.println("Init failed!");
+    delay(5000);
+  }
+  
+  sensor.setModel(MS5837::MS5837_30BA);
+  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
+}
+
+void loop() {
+  // Update pressure and temperature readings
+  sensor.read();
+
+  Serial.print("Pressure: "); 
+  Serial.print(sensor.pressure()); 
+  Serial.println(" mbar");
+  
+  Serial.print("Depth: "); 
+  Serial.print(sensor.depth()); 
+  Serial.println(" m");
+  
+  delay(1000);
+}
